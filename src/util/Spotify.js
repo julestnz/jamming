4959fb3aa4ae496bdf9f1f5194f7d12505b5d5f9
julestnz <julien.s.thomas@gmail.com>
2018-05-05 23:20:13 +1200

    chage

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,7 +3,7 @@ const clientId = '1a131bbede314fd6959a1505e97271c6';
 const clientSecret = '7157f929f7f8440fb86b20c1364aa5f6';
 const redirectURI = 'http://localhost:3000/callback/';
 
-let accessToken='';
+let accessToken;
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
